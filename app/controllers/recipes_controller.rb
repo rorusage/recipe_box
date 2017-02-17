0556fb77ee8b9f6fde7bbf9ef1fa971dfa03ed69
rorusage <rorusage@gmail.com>
2017-02-17 17:09:40 +0800

    Add create action in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,21 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_ingredient_step_params)
+
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_ingredient_step_params
+    params.require(:recipe).permit(:title, :content,
+                                   ingredient_attributes: [:name, :amount],
+                                   step_attributes: [:step_order, :content])
 
   end
 end
