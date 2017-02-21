864641e4fc497b79901d6bb8b3695574bfc4d8f5
rorusage <rorusage@gmail.com>
2017-02-21 17:43:41 +0800

    User can edit and destroy recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(recipe_ingredient_step_params)
 
     if @recipe.save
-      redirect_to recipes_path
+      redirect_to recipes_path, notice: "新增食譜成功！"
     else
       render :new
     end
@@ -21,6 +21,26 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_ingredient_step_params)
+      redirect_to recipes_path, notice: "修改食譜成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, alert: "食譜已刪除！"
   end
 
   private
