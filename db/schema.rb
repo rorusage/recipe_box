1a1690c2efeb84cb075c679cc757afa3f3943c34
rorusage <rorusage@gmail.com>
2017-02-21 18:03:51 +0800

    User can edit account and add name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221094420) do
+ActiveRecord::Schema.define(version: 20170221095619) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170221094420) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
