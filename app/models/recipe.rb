6ce000fe7482951182daf72ae82a989a368f8bdc
rorusage <rorusage@gmail.com>
2017-02-21 17:48:03 +0800

    add user_id to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,8 @@ class Recipe < ActiveRecord::Base
   has_many :steps, :dependent => :destroy
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :steps, reject_if: :all_blank, allow_destroy: true
+  validates :title, presence: true
+  validates :content, presence: true
+
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216232525) do
+ActiveRecord::Schema.define(version: 20170221094420) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170216232525) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "steps", force: :cascade do |t|
