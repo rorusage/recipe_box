eeb3b132b9a38e5419c0cbb355773be4ec39f356
rorusage <rorusage@gmail.com>
2017-02-21 17:54:50 +0800

    Only the owner can edit, update, destroy the recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,7 +10,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(recipe_ingredient_step_params)
+    @recipe = current_user.recipes.new(recipe_ingredient_step_params)
 
     if @recipe.save
       redirect_to recipes_path, notice: "新增食譜成功！"
@@ -24,11 +24,11 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
+    @recipe = current_user.recipes.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
+    @recipe = current_user.recipes.find(params[:id])
 
     if @recipe.update(recipe_ingredient_step_params)
       redirect_to recipes_path, notice: "修改食譜成功！"
@@ -38,7 +38,7 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
+    @recipe = current_user.recipes.find(params[:id])
     @recipe.destroy
     redirect_to recipes_path, alert: "食譜已刪除！"
   end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,8 @@ class Recipe < ActiveRecord::Base
   validates :content, presence: true
 
   belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+  def editable_by?(user)
+    user == owner
+  end
 end
